f8d7d8a5f694051161a54b7b59afa0dd61fb0f79
Juliano Barboza <julianodawid@gmail.com>
2018-01-01 18:25:39 -0300

    adds OcState Model

---
+++ app/OcState.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OcState extends Model
+{
+    public $timestamps = false;
+
+    /**
+     * Get the OcListQuery & OrdenCompra that belong to this state.
+     */
+    public function oc_list_query_orden_compras()
+    {
+        return $this->hasMany('App\OcListQueryOrdenCompra');
+    }
+}
