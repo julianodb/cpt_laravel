e18fd07f5616bc866d9c2d9842bd5e32f8642deb
Juliano Barboza <julianodawid@gmail.com>
2018-01-01 18:25:19 -0300

    adds OrdeCompra - OcListQuery relationship

--- app/OrdenCompra.php
+++ app/OrdenCompra.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class OrdenCompra extends Model
 {
     public $timestamps = false;
+
+    /**
+     * The OcListQuery's that contain the OrdenCompra
+     */
+    public function oc_list_queries()
+    {
+        return $this->belongsToMany('App\OcListQuery')
+            ->withPivot('name')
+            ->withPivot('oc_state_id')
+        	->using('App\OcListQueryOrdenCompra');
+    }
 }
