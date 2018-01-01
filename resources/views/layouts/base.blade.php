561d9285261f98339aadcda88cc6e3dcabe7c09c
Juliano Barboza <julianodawid@gmail.com>
2018-01-01 19:05:50 -0300

    adds jquery & jquery-ui to base layout

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,9 +5,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
-  <!--<link rel="stylesheet" href="{% static 'compras_publicas_transparentes/jquery-ui.min.css' %}">
-  <script src="{% static 'compras_publicas_transparentes/jquery.js' %}"></script>
-  <script src="{% static 'compras_publicas_transparentes/jquery-ui.min.js' %}"></script>-->
+
+  <link rel="stylesheet" href="{{ asset('css/jquery-ui.min.css') }}">
+  <script src="{{ asset('js/jquery.js') }}"></script>
+  <script src="{{ asset('js/jquery-ui.min.js') }}"></script>
 
   <title>{{$title}}</title>
 </head>
