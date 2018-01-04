eae1f52df22c35dadcde2a6d7d0c7a1c375f4c8b
Juliano Barboza <julianodawid@gmail.com>
2018-01-04 18:25:37 -0300

    improves OcListQuery happy path test

--- tests/Unit/OcListQueryTest.php
+++ tests/Unit/OcListQueryTest.php
@@ -5,38 +5,27 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Facades\App\Util\MercadoPublico;
 
 class TestOcListQuery extends TestCase
 {
     use RefreshDatabase;
-
-    public function setUp() {
-        parent::setup();
-        $this->sample = file_get_contents("tests/oclistquery.json");
-        //$json = json_decode($this->sample);
-    }
     
     /**
      * Happy path for storing new OcListQuery.
      *
      * @return void
      */
-    public function testStoreOcListQuery()
+    public function testStoreOcListQueryHappyPath()
     {
-        MercadoPublico::shouldReceive('get')->andReturn($this->sample);
-        $response = $this->post('/oc_list_queries',['date'=>'01/01/2017']);
-        $response->assertStatus(302);
-        $response->assertRedirect('oc_list_queries');
+        $sample = file_get_contents("tests/oclistquery.json");
+        $json = json_decode($sample);
 
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testTrue()
-    {
-        $this->assertTrue(true);
+        MercadoPublico::shouldReceive('get')->andReturn($sample);
+        $response = $this->followingRedirects()->post('/oc_list_queries',['date'=>'01/01/2017']);
+        $response->assertSuccessful();
+        $response->assertSee('01/01/2017');
+        $response->assertSee(strval($json->Cantidad));
+
     }
 }
