3b89aac1d84a84908d420471b439ec6104e8cc4f
Juliano Barboza <julianodawid@gmail.com>
2018-01-11 23:02:51 -0300

    adds test for NULL Financiamiento

--- tests/Unit/OcItemQueryTest.php
+++ tests/Unit/OcItemQueryTest.php
@@ -57,4 +57,28 @@ class TestOcItemQuery extends TestCase
         $response->assertSee('success'); // success BS alert with message
 
     }
+
+    /**
+     * Should accept json with financiamiento=NULL.
+     *
+     * @return void
+     */
+    public function testMissingFinancing()
+    {
+        $sample = file_get_contents("tests/ocitemquery.json");
+        $json = json_decode($sample);
+        $json->Listado[0]->Financiamiento = NULL;
+        $sample = json_encode($json);
+
+        MercadoPublico::shouldReceive('get')->andReturn($sample);
+        $response = $this->followingRedirects()->post('/oc_item_queries',
+        	['code'=>'CODE-HP-PATH']);
+        $response->assertSuccessful();
+        $response->assertSee('CODE-HP-PATH');
+        $response->assertSee(strval($json->Listado[0]->Nombre));
+        $response->assertSee(OcType::all()->where('code',$json->Listado[0]->Tipo)
+        	->first()->name);
+        $response->assertSee('success'); // success BS alert with message
+
+    }
 }
